79bb342841ef0a93bc045c8f967cd853b068b92f
Radiant28 <wwwanonymouswww372@gmail.com>
2024-03-24 18:05:22 +0300

    2525

--- Nerdnest/Pages/Clas/Index.cshtml.cs
+++ Nerdnest/Pages/Clas/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Nerdnest.Pages.Login
+namespace Nerdnest.Pages.Clas
 {
     public class IndexModel : PageModel
     {
--- Nerdnest/Pages/Login/Professors.cs
+++ Nerdnest/Pages/Login/Professors.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Nerdnest.Pages.Login
+namespace Nerdnest.Pages.Login.Professors
 {
     public class IndexModel : PageModel
     {
